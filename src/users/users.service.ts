70bfeeba746f66ab1af86dc64212e219e6e3102f
farukmuminovic <faruk_muminovic@hotmail.com>
2024-04-04 00:06:08 +0200

    Added database migrations and fixed hashing undefined password in update user method

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -29,12 +29,13 @@ export class UsersService {
   }
 
   async update(_id: string, updateUserInput: UpdateUserInput) {
+    if (updateUserInput.password)
+      await this.hashPassword(updateUserInput.password);
     return this.usersRepository.findOneAndUpdate(
       { _id },
       {
         $set: {
           ...updateUserInput,
-          password: await this.hashPassword(updateUserInput.password),
         },
       },
     );
