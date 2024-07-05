c60fb0b59a494af0c387f7d4e6f321f40eddc21f
farukmuminovic <faruk_muminovic@hotmail.com>
2024-07-06 00:11:54 +0200

    Added: enable cors in main.ts

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
   app.useGlobalPipes(new ValidationPipe());
   app.useLogger(app.get(Logger));
+  app.enableCors();
   app.use(cookieParser());
   const configService = app.get(ConfigService);
   await app.listen(configService.getOrThrow('PORT'));
